0cb66aea5bfd975ad9ba7c084a1abab532e03cdf
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-14 05:16:50 +0300

    commit tree 073c78060545f4e1f09fcc41d2345d5b4c3fadf6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+//Your code goes here
+// document.addEventListener("DoMContentLoaded",() => {
+//     const pTags = document.getElementById("text");
+//     pTags.textContent = "This is really cool!";
+
+// });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pTags = document.getElementById("text");
+    pTags.textContent = "This is really cool!";
+});
